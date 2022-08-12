6c79334d4ec27416e1b110c52a75bc746075285a
ArbenitBytyqi <arbenitbytyqi7@gmail.com>
2022-08-12 18:05:36 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Arbi's Todos</h1>
+      <input type="text" placeholder="Write your todo here"></input>
+      <button>Submit</button>
+      <button>Delete Todo</button>
+    </div>
+  );
+}
+
+export default App;
