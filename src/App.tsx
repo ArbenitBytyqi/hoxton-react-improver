58bb28ba472d69387d56d9518efafe696cdaf441
ArbenitBytyqi <arbenitbytyqi7@gmail.com>
2022-08-13 22:51:44 +0200

    State and Map finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,53 @@
+import { useState } from "react";
+import "./App.css";
+
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      content: "Walk the dog",
+      string: "8 am",
+      completed: true,
+    },
+    {
+      id: 2,
+      content: "Write some musi",
+      string: "6 pm",
+      completed: true,
+    },
+    {
+      id: 3,
+      content: "Go to the gym",
+      string: "8 pm",
+      completed: true,
+    },
+    {
+      id: 4,
+      content: "Finish side-project tasks",
+      string: "9pm",
+      completed: false,
+    },
+    {
+      id: 5,
+      content: "Edit the video",
+      string: "11 pm",
+      completed: false,
+    },
+  ]);
+
   return (
     <div className="App">
       <h1>Arbi's Todos</h1>
       <input type="text" placeholder="Write your todo here"></input>
       <button>Submit</button>
-      <button>Delete Todo</button>
+      <div className="todoList">
+        {todos.map((todo) => (
+          <li>
+            <h3>{todo.content}</h3>
+            <h2>{todo.string}</h2>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
